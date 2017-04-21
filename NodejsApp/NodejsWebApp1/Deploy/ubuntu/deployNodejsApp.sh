5a58d4c1480bde64a8b119cc132239308976c7bc
Bishal Prasad <biprasad@microsoft.com>
2017-04-21 17:54:58 +0530

    adding ubuntu deploy scruipts

---
+++ NodejsApp/NodejsWebApp1/Deploy/ubuntu/deployNodejsApp.sh
@@ -0,0 +1,13 @@
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo apt-get install nginx -y
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp node-app-nginx-config /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/node-app-nginx-config /etc/nginx/sites-enabled/node-app-nginx-config
+sudo service nginx restart
+
+sudo npm install pm2 -g
+sudo pm2 start server.js
+sudo pm2 save
+sudo pm2 startup
