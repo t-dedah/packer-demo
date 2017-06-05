300e1f23c7549e1907322fe3e720c0920132bcfe
Bishal Prasad <biprasad@microsoft.com>
2017-06-05 15:30:17 +0530

    Adding rhel deploy files - currently just installs nginx

---
+++ NodejsApp/NodejsWebApp1/Deploy/rhel/deployNodejsApp.sh
@@ -0,0 +1,7 @@
+sudo yum install epel-release
+sudo yum install ansible
+
+mkdir -p $HOME/ansible-nginx/tasks/
+script_dir=$(dirname "$0")
+cp $script_dir/deploy.yml $HOME/ansible-nginx/deploy.yml
+cp $script_dir/install_nginx.yml $HOME/ansible-nginx/tasks/install_nginx.yml
