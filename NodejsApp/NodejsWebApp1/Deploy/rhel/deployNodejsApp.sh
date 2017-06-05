ab6517b3fb0114bb1b87318fb259eeb04fa72c2e
Bishal Prasad <biprasad@microsoft.com>
2017-06-05 16:04:16 +0530

    fixes

--- NodejsApp/NodejsWebApp1/Deploy/rhel/deployNodejsApp.sh
+++ NodejsApp/NodejsWebApp1/Deploy/rhel/deployNodejsApp.sh
@@ -5,3 +5,5 @@ mkdir -p $HOME/ansible-nginx/tasks/
 script_dir=$(dirname "$0")
 cp $script_dir/deploy.yml $HOME/ansible-nginx/deploy.yml
 cp $script_dir/install_nginx.yml $HOME/ansible-nginx/tasks/install_nginx.yml
+
+sudo ansible-playbook $HOME/ansible-nginx/deploy.yml
